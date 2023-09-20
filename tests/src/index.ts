88ed932c33fa6ecb2812b98c4bfbe2c3b73b516d
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2023-09-20 14:20:27 +0400

    fix showFooter not working

--- tests/src/index.ts
+++ tests/src/index.ts
@@ -2,11 +2,12 @@ import { Mahal } from "mahal";
 import App from "@/app.mahal";
 import config from "~/config/env";
 import { BulmaPlugin } from "mahal-bulma";
+import "bulma";
 
 
 const app = new Mahal(App, '#app');
 app.extend.plugin(BulmaPlugin);
- 
+
 // set config to be available globally
 app.global.env = config;
 
